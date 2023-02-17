0747cb74656583bdf5739fbaac863bee2f041d51
Mostakimw <scientist.expert@gmail.com>
2023-02-18 02:11:17 +0600

    all event handler and calculation completed

--- javascript/app.js
+++ javascript/app.js
@@ -1,71 +1,40 @@
-let serial = 1;
-
-// function for taking innerText from html element
-function innerText(id) {
-  const element = document.getElementById(id).innerText;
-  return element;
-}
-// function for taking value from input
-function valueOfInput(id) {
-  const value = document.getElementById(id).value;
-  return value;
-}
+// ! triangle event handler
+document.getElementById("triangle-btn").addEventListener("click", function () {
+  calculateArea("triangle-title", "triangle-base", "triangle-height");
+});
 
 // ! rectangular event handler
 document.getElementById("rectangle-btn").addEventListener("click", function () {
-  const rectangleName = innerText("rectangle-title");
-  const rectangleWidth = valueOfInput("rectangle-width");
-  const rectangleLength = valueOfInput("rectangle-length");
-  const area = rectangleWidth * rectangleLength;
-  validationCheck(rectangleWidth, rectangleLength, rectangleName, area);
-});
-
-// ! triangle event handler
-document.getElementById("triangle-btn").addEventListener("click", function () {
-  const triangleName = document.getElementById("triangle-title").innerText;
-  const base = document.getElementById("triangle-base").value;
-  const height = document.getElementById("triangle-height").value;
-  const area = 0.5 * base * height;
-  validationCheck(base, height, triangleName, area);
-});
-
-// function for validation check
-function validationCheck(value1, value2, geometricName, area) {
-  if (value1 < 0 || value2 < 0 || value1 == "" || value2 == "") {
-    alert("Value should be a positive number.");
-  } else {
-    calculationDisplay(geometricName, area);
-  }
-}
-
-// function for showing calculate data
-function calculationDisplay(name, area) {
-  const areaCalculationContainer = document.getElementById("show-data");
-  const tr = document.createElement("tr");
-  tr.innerHTML = `
-    <td>${serial}</td>
-    <td>${name}</td>
-    <td>${area + "cm"}</td>
-    <td>${centimeterToMeterBtn()}</td>
-    `;
-  areaCalculationContainer.appendChild(tr);
-  serial += 1;
-}
-
-//! function for cm to m button function
-function centimeterToMeterBtn() {
-  const button = document.createElement("button");
-  button.innerText = "Convert to m";
-  button.classList.add(
-    "bg-blue-500",
-    "hover:bg-blue-700",
-    "text-white",
-    "font-bold",
-    "py-1",
-    "px-2",
-    "rounded"
+  calculateAreaForRecAndPara(
+    "rectangle-title",
+    "rectangle-width",
+    "rectangle-length"
   );
-  const buttonContainer = document.createElement("div");
-  buttonContainer.appendChild(button);
-  return buttonContainer.innerHTML;
-}
+});
+
+// ! Parallelogram event handler
+document
+  .getElementById("parallelogram-btn")
+  .addEventListener("click", function () {
+    calculateAreaForRecAndPara(
+      "parallelogram-title",
+      "parallelogram-base",
+      "parallelogram-height"
+    );
+  });
+
+// ! Rhombus event handler
+
+document.getElementById("rhombus-btn").addEventListener("click", function () {
+  calculateArea("rhombus-title", "rhombus-base", "rhombus-height");
+});
+
+// ! pentagon event handler
+document.getElementById("pentagon-btn").addEventListener("click", function () {
+  calculateArea("pentagon-title", "pentagon-perimeter", "pentagon-apothem ");
+});
+
+// ! ellipse event handler
+document.getElementById("ellipse-btn").addEventListener("click", function () {
+  calculateAreaForEllipse("ellipse-title", "ellipse-x-axis", "ellipse-y-axis");
+});
---
+++ javascript/common.js
@@ -0,0 +1,79 @@
+// ! function for taking innertext from html element
+function titleInnerText(id) {
+  const element = document.getElementById(id).innerText;
+  return element;
+}
+
+// ! function for taking value from input
+function valueOfInput(id) {
+  const value = document.getElementById(id).value;
+  return value;
+}
+
+let serial = 1;
+// ! function for showing calculate data
+function calculationDisplay(name, area) {
+  const areaCalculationContainer = document.getElementById("show-data");
+  const tr = document.createElement("tr");
+  tr.innerHTML = `
+      <td>${serial + ". " + name}</td>    
+      <td>${area + "cm"}</td>
+      <td>${centimeterToMeterBtn()}</td>
+      `;
+  areaCalculationContainer.appendChild(tr);
+  serial += 1;
+}
+
+//! function for cm to m button function
+function centimeterToMeterBtn() {
+  const button = document.createElement("button");
+  button.innerText = "Convert to m";
+  button.classList.add(
+    "bg-blue-500",
+    "hover:bg-blue-700",
+    "text-white",
+    "font-bold",
+    "py-1",
+    "px-2",
+    "rounded"
+  );
+  const buttonContainer = document.createElement("div");
+  buttonContainer.appendChild(button);
+  return buttonContainer.innerHTML;
+}
+
+// ! function for validation check
+function validationCheck(value1, value2, geometricName, area) {
+  if (value1 < 0 || value2 < 0 || value1 == "" || value2 == "") {
+    alert("All Values should be a positive number.");
+  } else {
+    calculationDisplay(geometricName, area);
+  }
+}
+
+// ! function for calculate triangle, Rhombus, Pentagon area
+function calculateArea(nameId, value1Id, value2Id) {
+  const name = document.getElementById(nameId).innerText;
+  const value1 = document.getElementById(value1Id).value;
+  const value2 = document.getElementById(value2Id).value;
+  const area = 0.5 * value1 * value2;
+  validationCheck(value1, value2, name, area);
+}
+
+// ! function for calculate rectangular, Parallelogram area
+function calculateAreaForRecAndPara(nameId, value1Id, value2Id) {
+  const name = document.getElementById(nameId).innerText;
+  const value1 = document.getElementById(value1Id).value;
+  const value2 = document.getElementById(value2Id).value;
+  const area = value1 * value2;
+  validationCheck(value1, value2, name, area);
+}
+
+// ! function for calculate ellipse area
+function calculateAreaForEllipse(nameId, value1Id, value2Id) {
+  const name = document.getElementById(nameId).innerText;
+  const value1 = document.getElementById(value1Id).value;
+  const value2 = document.getElementById(value2Id).value;
+  const area = (3.14 * value1 * value2).toFixed(2);
+  validationCheck(value1, value2, name, area);
+}
